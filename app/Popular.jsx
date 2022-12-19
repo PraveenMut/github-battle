247341ddb6c4e3fbd7ae66fb850bb93824d66d5f
Praveen Muthu <praveen.mut@gmail.com>
2022-12-19 13:02:18 +1100

    Store the actual selected value into state

--- app/Popular.jsx
+++ app/Popular.jsx
@@ -1,16 +1,38 @@
 import * as React from "react";
 
 export default class Popular extends React.Component {
-    render() { 
-        const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
-        return (
-            <select>
-                {
-                    languages.map((lang) => (
-                        <option key={lang} value={lang}>{lang}</option>
-                    ))
-                }
-            </select>
-        )
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        selectedLanguage: {}
+     }
+
+     this.updateLanguage = this.updateLanguage.bind(this);
+     this.languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+  }
+
+  updateLanguage(lang) {
+    this.setState(({ selectedLanguage }) => ({
+        selectedLanguage: lang
+    }))
+  }
+
+  render() {
+    return (
+      <main>  
+      <select 
+      onChange={(e) => this.updateLanguage(e.target.value)} 
+      value={this.state.selectedLanguage}
+      >
+        {this.languages.map((lang) => (
+          <option key={lang} value={lang}>
+            {lang}
+          </option>
+        ))}
+      </select>
+      {JSON.stringify(this.state, null, 2)}
+      </main>
+    );
+  }
 }
