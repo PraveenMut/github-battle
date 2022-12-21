75a4723d573979d96970d44551559d2a7e8e8437
Praveen Muthu <praveen.mut@gmail.com>
2022-12-21 11:33:54 +1100

    Create the `ResultsCard` component.
    And integrate it to the `Battle` component.

--- app/Results.jsx
+++ app/Results.jsx
@@ -1,19 +1,78 @@
 import * as React from "react";
+import ResultsCard from "./ResultsCard";
 import { battle } from "./utils/api/api";
 
 export default class Results extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            winner: null,
+            runnerup: null,
+            error: null,
+            loading: true,
+        }
+    }
     async componentDidMount() {
         const { playerOne, playerTwo } = this.props;
         
-        const result = await battle([playerOne, playerTwo])
-        console.log(result)
+        try {
+            const [winner, runnerup] = await battle([playerOne, playerTwo])
+            this.setState({
+                winner,
+                runnerup,
+                error: null,
+                loading: false,
+            });
+        } catch(ex) {
+            this.setState({
+                error: ex.message,
+                loading: false,
+            })
+        }
     }
     render() {
+        if (this.state.loading) {
+            return <i>Loading...</i>
+        }
+
+        if (this.state.error) {
+            return  <p className="text-center error">{error}</p>;
+        }
+        
         return (
-            <div>
-                Results
-                <pre>{JSON.stringify(this.props, null, 2)}</pre>
+            <main className="animate-in stack main-stack">
+            <div className="split">
+              <h1>Results</h1>
             </div>
+            <section className="grid">
+              <article className="results-container">
+                <ResultsCard profile={this.state.winner.profile} />
+                <p className="results">
+                  <span>
+                    {this.state.winner.score === this.state.runnerup.score ? "Tie" : "Winner"}{" "}
+                    {this.state.winner.score.toLocaleString()}
+                  </span>
+                  {this.state.winner.score !== this.state.runnerup.score && (
+                    <img
+                      width={80}
+                      src="https://ui.dev/images/certificate.svg"
+                      alt="Certificate"
+                    />
+                  )}
+                </p>
+              </article>
+              <article className="results-container">
+                <ResultsCard profile={this.state.runnerup.profile} />
+                <p className="results">
+                  <span>
+                    {this.state.winner.score === this.state.runnerup.score ? "Tie" : "Loser"}{" "}
+                    {this.state.runnerup.score.toLocaleString()}
+                  </span>
+                </p>
+              </article>
+            </section>
+          </main>
         )
     }
 }
---
+++ app/ResultsCard.jsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+
+const ResultsCard = ({ profile }) => {
+  const {
+    login,
+    avatar_url,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    location,
+    company,
+  } = profile;
+
+  return (
+    <div className="card bg-light">
+      <header className="split">
+        <div>
+          <h4>
+            <a href={html_url}>{login}</a>
+          </h4>
+          <p>{location || "unknown"}</p>
+        </div>
+        <img
+          className="avatar large"
+          src={avatar_url}
+          alt={`Avatar for ${login}`}
+        />
+      </header>
+      <ul className="stack">
+        <li className="split">
+          <span>Name:</span> <span>{login || "n/a"}</span>
+        </li>
+        <li className="split">
+          <span>Company:</span> <span>{company || "n/a"}</span>
+        </li>
+        <li className="split">
+          <span>Followers:</span> <span>{followers}</span>
+        </li>
+        <li className="split">
+          <span>Following:</span> <span>{following}</span>
+        </li>
+        <li className="split">
+          <span>Repositories:</span> <span>{public_repos}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ResultsCard;
