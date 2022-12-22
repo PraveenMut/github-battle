1ce41e81046d5b8d8ed50a8aa947821678ad28e3
Praveen Muthu <praveen.mut@gmail.com>
2022-12-22 13:38:45 +1100

    Create initial implementation of links

---
+++ app/Navbar.jsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import { NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
+import { sunIcon, moonIcon } from "./icon";
+
+const Navbar = (props) => (
+  <nav className="split">
+    <NavLink
+      to="/"
+      className={({ isActive }) => "nav-link" + (isActive ? "active" : "")}
+    >
+      GitHub Battle
+    </NavLink>
+    <ul className="row">
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive }) => "nav-link" + (isActive ? "active" : "")}
+        >
+          Popular
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/battle"
+          className={({ isActive }) => "nav-link" + (isActive ? "active" : "")}
+        >
+          Battle
+        </NavLink>
+      </li>
+      <li>
+        <button className="btn secondary icon" onClick={props.toggleTheme}>
+          {props.theme === "light" ? moonIcon : sunIcon}
+        </button>
+      </li>
+    </ul>
+  </nav>
+);
+
+Navbar.PropTypes = {
+  theme: PropTypes.string.isRequired,
+  toggletheme: PropTypes.func.isRequired,
+};
+
+export default Navbar;
--- app/index.jsx
+++ app/index.jsx
@@ -4,14 +4,32 @@ import Battle from "./Battle";
 import "./index.css";
 import Popular from "./Popular";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
 
 // JSX Style of React
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: "light",
+    };
+
+    this.toggleTheme = this.toggleTheme.bind(this);
+  }
+
+  toggleTheme() {
+    this.setState((prevState) => ({
+      theme: prevState.theme == "light" ? "dark" : "light",
+    }));
+  }
+
   render() {
     return (
       <BrowserRouter>
-        <div className="light">
+        <div className={this.state.theme}>
           <div className="container">
+            <Navbar theme={this.state.theme} toggleTheme={this.toggleTheme}/>
             <Routes>
               <Route path="/" element={<Popular />} />
               <Route path="/battle" element={<Battle />} />
